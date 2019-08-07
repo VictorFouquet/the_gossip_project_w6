cb0123f36b06b8324a9e064843944c8f73edfc5f
victorfouquet <victor.fouquet@gmail.com>
2019-08-07 13:18:53 +0200

    Hot Gossip

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_124007) do
+ActiveRecord::Schema.define(version: 2019_08_07_105252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "gossips", force: :cascade do |t|
     t.string "title"
@@ -32,7 +38,10 @@ ActiveRecord::Schema.define(version: 2019_08_05_124007) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
   end
 
   add_foreign_key "gossips", "users"
+  add_foreign_key "users", "cities"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,28 @@
 
 Gossip.destroy_all
 User.destroy_all
+City.destroy_all
+cities = ["Paris", "Rennes", "Nantes", "Rennes", "Tours", "Toulouse", "Montpellier", "Strasbourg", "Marseille", "Lille"]
+n = 0
+
+10.times do 
+	City.create!(
+		name: cities[n]
+		)
+	n += 1
+end
+
+City.create!(
+	name: "I know yours already dude..."
+	)
+
 5.times do
 	User.create!(
 		first_name: Faker::Name.first_name,
 		last_name: Faker::Name.last_name,
 		email: Faker::Internet.email, 
-		description: Faker::Movies::Lebowski.quote
+		description: Faker::Movies::Lebowski.quote,
+		city: City.all.sample
 		)
 end
 
@@ -22,7 +38,7 @@ end
 		title: Faker::Hipster.word,
 		content: Faker::Quote.most_interesting_man_in_the_world,
 		date: Faker::Date.between(from: 60.days.ago, to: Date.today).strftime("Créé le %d/%m/%Y"),
-		user: User.find(rand(User.first.id..User.last.id))
+		user: User.all.sample
 		)
 end
 
@@ -30,5 +46,6 @@ User.create!(
 	first_name: "Ano",
 	last_name: "Nymous",
 	email: "ano.nymous@gmail.com", 
-	description: " We are Anonymous. We are Legion. We do not forgive. We do not forget. Expect us." 
+	description: " We are Anonymous. We are Legion. We do not forgive. We do not forget. Expect us.", 
+	city: City.find_by(name: "I know yours already dude...")
 	)
